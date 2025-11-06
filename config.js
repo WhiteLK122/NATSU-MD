a6e8ba8129cff537460edf9465bd2832dbf5285b
WhiteLK122 <gimshandilisha95@gmail.com>
2025-11-06 19:48:50 +0530

    N O N B U T T O N . J S

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PUT YOUR SESSION ID",
+SESSION_ID: process.env.SESSION_ID || "NATSU-ID~e50735022d39a15e",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
